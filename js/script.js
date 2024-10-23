9d868a9a31d5b882e1fff5c43e8a9340c7e1cafc
Alejo-42 <froy2004xd@gmail.com>
2024-10-23 10:50:23 -0500

    boton agregado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log(document)
+const $body = document.querySelector("#inicio")
+const $MD = document.querySelector("#MD")
+function modoOscuro() {
+    $body.classList.toggle("modoOscuro")
+    $MD.textContent = $MD.textContent === "MODO OSCURO" ? "MODO NORMAL" : "MODO OSCURO"
+
+}
+
+
+$MD.addEventListener("click",modoOscuro)
