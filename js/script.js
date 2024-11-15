27ab46544f339a3920c2f644c15522fada104c03
Alejo <froy2004xd@gmail.com>
2024-11-15 17:09:42 -0500

    xd

--- js/script.js
+++ js/script.js
@@ -1,20 +1,10 @@
 const $body = document.querySelector("#inicio")
 const $MD = document.querySelector("#MD")
 
-const carousel = document.querySelector('.carousel');
-const albumButton = document.querySelector('.album-button');
-const carouselItems = carousel.querySelectorAll('.carousel-item');
-// Suponiendo que cada diapositiva tiene un atributo data-album
-carouselItems.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    const albumId = item.dataset.album;
-    albumButton.textContent = `Ver álbum ${albumId}`;
-  });
-});function modoOscuro() {
+function modoOscuro() {
     $body.classList.toggle("modoOscuro")
     $MD.textContent = $MD.textContent === "MODO OSCURO" ? "MODO NORMAL" : "MODO OSCURO"
 
 }
-
 
 $MD.addEventListener("click",modoOscuro)
