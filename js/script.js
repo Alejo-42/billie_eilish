91b55702154e4b569168069c050b4cc7f6b8eae4
Alejo-42 <froy2004xd@gmail.com>
2024-12-02 09:38:14 -0500

    Boton hamburguesa hecho

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 const $body = document.querySelector("body")
 const $MD = document.querySelector("#MD")
 
+const $abrir = document.querySelector(".abrir")
+const $cerrar = document.querySelector(".cerrar")
+const $albumes = document.querySelector(".btn-albumes")
+
 function modoOscuro() {
     $body.classList.toggle("modoOscuro")
     $MD.textContent = $MD.textContent === "DARK MODE" ? "NORMAL MODE" : "DARK MODE"
 }
+function abrir() {
+    $albumes.classList.add("visible")
+    $abrir.classList.add("ocultarBoton")
+}
+function cerrar() {
+    $albumes.classList.remove("visible")
+    $abrir.classList.remove("ocultarBoton")
+}
 
 $MD.addEventListener("click",modoOscuro)
+
+$abrir.addEventListener("click", abrir)
+$cerrar.addEventListener("click",cerrar)
