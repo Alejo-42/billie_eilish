aa4d6c8373ebdecf4a6fe88d0e68fb6cd760aedb
Alejo <froy2004xd@gmail.com>
2025-01-29 13:39:25 -0500

    hte responsivo falta resto

---
+++ components/hte/script.js
@@ -0,0 +1,271 @@
+// Getting Older (1)
+const botonMostrar1 = document.querySelector('#mostrar1');
+const significado1 = document.querySelector('#meaning1');
+
+function mostrarOcultarDescripcion1() {
+    if (significado1.style.display === 'none') {
+        significado1.style.display = 'block';
+        botonMostrar1.textContent = 'Ocultar';
+    } else {
+        significado1.style.display = 'none';
+        botonMostrar1.textContent = 'Significado';
+    }
+}
+
+botonMostrar1.addEventListener('click', mostrarOcultarDescripcion1)
+
+// I Didn't Change My Number (2)
+const botonMostrar2 = document.querySelector('#mostrar2');
+const significado2 = document.querySelector('#meaning2');
+
+function mostrarOcultarDescripcion2() {
+    if (significado2.style.display === 'none') {
+        significado2.style.display = 'block';
+        botonMostrar2.textContent = 'Ocultar';
+    } else {
+        significado2.style.display = 'none';
+        botonMostrar2.textContent = 'Significado';
+    }
+}
+
+botonMostrar2.addEventListener('click', mostrarOcultarDescripcion2);
+
+// Billie Bossa Nova (3)
+const botonMostrar3 = document.querySelector('#mostrar3');
+const significado3 = document.querySelector('#meaning3');
+
+function mostrarOcultarDescripcion3() {
+    if (significado3.style.display === 'none') {
+        significado3.style.display = 'block';
+        botonMostrar3.textContent = 'Ocultar';
+    } else {
+        significado3.style.display = 'none';
+        botonMostrar3.textContent = 'Significado';
+    }
+}
+
+botonMostrar3.addEventListener('click', mostrarOcultarDescripcion3);
+
+// my future (4)
+
+const botonMostrar4 = document.querySelector('#mostrar4');
+const significado4 = document.querySelector('#meaning4');
+
+function mostrarOcultarDescripcion4() {
+    if (significado4.style.display === 'none') {
+        significado4.style.display = 'block';
+        botonMostrar4.textContent = 'Ocultar';
+    } else {
+        significado4.style.display = 'none';
+        botonMostrar4.textContent = 'Significado';
+    }
+}
+
+botonMostrar4.addEventListener('click', mostrarOcultarDescripcion4);
+
+// Oxytocin (5) <3
+
+const botonMostrar5 = document.querySelector('#mostrar5');
+const significado5 = document.querySelector('#meaning5');
+
+function mostrarOcultarDescripcion5() {
+    if (significado5.style.display === 'none') {
+        significado5.style.display = 'block';
+        botonMostrar5.textContent = 'Ocultar';
+    } else {
+        significado5.style.display = 'none';
+        botonMostrar5.textContent = 'Significado';
+    }
+}
+
+botonMostrar5.addEventListener('click', mostrarOcultarDescripcion5);
+
+// GOLDWING (6)
+
+const botonMostrar6 = document.querySelector('#mostrar6');
+const significado6 = document.querySelector('#meaning6');
+
+function mostrarOcultarDescripcion6() {
+    if (significado6.style.display === 'none') {
+        significado6.style.display = 'block';
+        botonMostrar6.textContent = 'Ocultar';
+    } else {
+        significado6.style.display = 'none';
+        botonMostrar6.textContent = 'Significado';
+    }
+}
+
+botonMostrar6.addEventListener('click', mostrarOcultarDescripcion6);
+
+// Lost Cause (7)
+
+const botonMostrar7 = document.querySelector('#mostrar7');
+const significado7 = document.querySelector('#meaning7');
+
+function mostrarOcultarDescripcion7() {
+    if (significado7.style.display === 'none') {
+        significado7.style.display = 'block';
+        botonMostrar7.textContent = 'Ocultar';
+    } else {
+        significado7.style.display = 'none';
+        botonMostrar7.textContent = 'Significado';
+    }
+}
+
+botonMostrar7.addEventListener('click', mostrarOcultarDescripcion7);
+
+
+// Halley's Comet (8)
+
+const botonMostrar8 = document.querySelector('#mostrar8');
+const significado8 = document.querySelector('#meaning8');
+
+function mostrarOcultarDescripcion8() {
+    if (significado8.style.display === 'none') {
+        significado8.style.display = 'block';
+        botonMostrar8.textContent = 'Ocultar';
+    } else {
+        significado8.style.display = 'none';
+        botonMostrar8.textContent = 'Significado';
+    }
+}
+
+botonMostrar8.addEventListener('click', mostrarOcultarDescripcion8);
+
+// Not My Responsability (9)
+
+const botonMostrar9 = document.querySelector('#mostrar9');
+const significado9 = document.querySelector('#meaning9');
+
+function mostrarOcultarDescripcion9() {
+    if (significado9.style.display === 'none') {
+        significado9.style.display = 'block';
+        botonMostrar9.textContent = 'Ocultar';
+    } else {
+        significado9.style.display = 'none';
+        botonMostrar9.textContent = 'Significado';
+    }
+}
+
+botonMostrar9.addEventListener('click', mostrarOcultarDescripcion9);
+
+// OverHeated (10)
+
+const botonMostrar10 = document.querySelector('#mostrar10');
+const significado10 = document.querySelector('#meaning10');
+
+function mostrarOcultarDescripcion10() {
+    if (significado10.style.display === 'none') {
+        significado10.style.display = 'block';
+        botonMostrar10.textContent = 'Ocultar';
+    } else {
+        significado10.style.display = 'none';
+        botonMostrar10.textContent = 'Significado';
+    }
+}
+
+botonMostrar10.addEventListener('click', mostrarOcultarDescripcion10);
+
+// Everybody Dies (11)
+
+const botonMostrar11 = document.querySelector('#mostrar11');
+const significado11 = document.querySelector('#meaning11');
+
+function mostrarOcultarDescripcion11() {
+    if (significado11.style.display === 'none') {
+        significado11.style.display = 'block';
+        botonMostrar11.textContent = 'Ocultar';
+    } else {
+        significado11.style.display = 'none';
+        botonMostrar11.textContent = 'Significado';
+    }
+}
+
+botonMostrar11.addEventListener('click', mostrarOcultarDescripcion11);
+
+// Your Power (12)
+
+const botonMostrar12 = document.querySelector('#mostrar12');
+const significado12 = document.querySelector('#meaning12');
+
+function mostrarOcultarDescripcion12() {
+    if (significado12.style.display === 'none') {
+        significado12.style.display = 'block';
+        botonMostrar12.textContent = 'Ocultar';
+    } else {
+        significado12.style.display = 'none';
+        botonMostrar12.textContent = 'Significado';
+    }
+}
+
+botonMostrar12.addEventListener('click', mostrarOcultarDescripcion12);
+
+// NDA (13)
+
+const botonMostrar13 = document.querySelector('#mostrar13');
+const significado13 = document.querySelector('#meaning13');
+
+function mostrarOcultarDescripcion13() {
+    if (significado13.style.display === 'none') {
+        significado13.style.display = 'block';
+        botonMostrar13.textContent = 'Ocultar';
+    } else {
+        significado13.style.display = 'none';
+        botonMostrar13.textContent = 'Significado';
+    }
+}
+
+botonMostrar13.addEventListener('click', mostrarOcultarDescripcion13);
+
+// Therefore I Am (14)
+
+const botonMostrar14 = document.querySelector('#mostrar14');
+const significado14 = document.querySelector('#meaning14');
+
+function mostrarOcultarDescripcion14() {
+    if (significado14.style.display === 'none') {
+        significado14.style.display = 'block';
+        botonMostrar14.textContent = 'Ocultar';
+    } else {
+        significado14.style.display = 'none';
+        botonMostrar14.textContent = 'Significado';
+    }
+}
+
+botonMostrar14.addEventListener('click', mostrarOcultarDescripcion14);
+
+
+// Happier Than Ever (15)
+
+const botonMostrar15 = document.querySelector('#mostrar15');
+const significado15 = document.querySelector('#meaning15');
+
+function mostrarOcultarDescripcion15() {
+    if (significado15.style.display === 'none') {
+        significado15.style.display = 'block';
+        botonMostrar15.textContent = 'Ocultar';
+    } else {
+        significado15.style.display = 'none';
+        botonMostrar15.textContent = 'Significado';
+    }
+}
+
+botonMostrar15.addEventListener('click', mostrarOcultarDescripcion15);
+
+
+// Male Fantasy (16)
+
+const botonMostrar16 = document.querySelector('#mostrar16');
+const significado16 = document.querySelector('#meaning16');
+
+function mostrarOcultarDescripcion16() {
+    if (significado16.style.display === 'none') {
+        significado16.style.display = 'block';
+        botonMostrar16.textContent = 'Ocultar';
+    } else {
+        significado16.style.display = 'none';
+        botonMostrar16.textContent = 'Significado';
+    }
+}
+
+botonMostrar16.addEventListener('click', mostrarOcultarDescripcion16);
