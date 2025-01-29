e0a0c21d3dd360fa9140f771df8be042517fbbe4
Alejo <froy2004xd@gmail.com>
2025-01-29 11:47:38 -0500

    hmhas responsivo falta resto

---
+++ components/hmhas/script.js
@@ -0,0 +1,167 @@
+// SKINNY (1)
+const botonMostrar1 = document.querySelector('#mostrar1');
+const significado1 = document.querySelector('#meaning1');
+
+function mostrarOcultarDescripcion1() {
+    if (significado1.style.display === 'none') {
+        significado1.style.display = 'block';
+        botonMostrar1.textContent = 'Ocultar';
+    } else {
+        significado1.style.display = 'none';
+        botonMostrar1.textContent = 'Significado';
+    }
+}
+
+botonMostrar1.addEventListener('click', mostrarOcultarDescripcion1)
+
+// LUNCH (2)
+const botonMostrar2 = document.querySelector('#mostrar2');
+const significado2 = document.querySelector('#meaning2');
+
+function mostrarOcultarDescripcion2() {
+    if (significado2.style.display === 'none') {
+        significado2.style.display = 'block';
+        botonMostrar2.textContent = 'Ocultar';
+    } else {
+        significado2.style.display = 'none';
+        botonMostrar2.textContent = 'Significado';
+    }
+}
+
+botonMostrar2.addEventListener('click', mostrarOcultarDescripcion2);
+
+// CHIHIRO (3)
+const botonMostrar3 = document.querySelector('#mostrar3');
+const significado3 = document.querySelector('#meaning3');
+
+function mostrarOcultarDescripcion3() {
+    if (significado3.style.display === 'none') {
+        significado3.style.display = 'block';
+        botonMostrar3.textContent = 'Ocultar';
+    } else {
+        significado3.style.display = 'none';
+        botonMostrar3.textContent = 'Significado';
+    }
+}
+
+botonMostrar3.addEventListener('click', mostrarOcultarDescripcion3);
+
+// BIRDS OF A FEATHER (4)
+
+const botonMostrar4 = document.querySelector('#mostrar4');
+const significado4 = document.querySelector('#meaning4');
+
+function mostrarOcultarDescripcion4() {
+    if (significado4.style.display === 'none') {
+        significado4.style.display = 'block';
+        botonMostrar4.textContent = 'Ocultar';
+    } else {
+        significado4.style.display = 'none';
+        botonMostrar4.textContent = 'Significado';
+    }
+}
+
+botonMostrar4.addEventListener('click', mostrarOcultarDescripcion4);
+
+// WILDFLOWER (5) <3
+
+const botonMostrar5 = document.querySelector('#mostrar5');
+const significado5 = document.querySelector('#meaning5');
+
+function mostrarOcultarDescripcion5() {
+    if (significado5.style.display === 'none') {
+        significado5.style.display = 'block';
+        botonMostrar5.textContent = 'Ocultar';
+    } else {
+        significado5.style.display = 'none';
+        botonMostrar5.textContent = 'Significado';
+    }
+}
+
+botonMostrar5.addEventListener('click', mostrarOcultarDescripcion5);
+
+// THE GREATEST (6)
+
+const botonMostrar6 = document.querySelector('#mostrar6');
+const significado6 = document.querySelector('#meaning6');
+
+function mostrarOcultarDescripcion6() {
+    if (significado6.style.display === 'none') {
+        significado6.style.display = 'block';
+        botonMostrar6.textContent = 'Ocultar';
+    } else {
+        significado6.style.display = 'none';
+        botonMostrar6.textContent = 'Significado';
+    }
+}
+
+botonMostrar6.addEventListener('click', mostrarOcultarDescripcion6);
+
+// L' AMOUR DE MA VIE (7)
+
+const botonMostrar7 = document.querySelector('#mostrar7');
+const significado7 = document.querySelector('#meaning7');
+
+function mostrarOcultarDescripcion7() {
+    if (significado7.style.display === 'none') {
+        significado7.style.display = 'block';
+        botonMostrar7.textContent = 'Ocultar';
+    } else {
+        significado7.style.display = 'none';
+        botonMostrar7.textContent = 'Significado';
+    }
+}
+
+botonMostrar7.addEventListener('click', mostrarOcultarDescripcion7);
+
+
+// THE DINER (8)
+
+const botonMostrar8 = document.querySelector('#mostrar8');
+const significado8 = document.querySelector('#meaning8');
+
+function mostrarOcultarDescripcion8() {
+    if (significado8.style.display === 'none') {
+        significado8.style.display = 'block';
+        botonMostrar8.textContent = 'Ocultar';
+    } else {
+        significado8.style.display = 'none';
+        botonMostrar8.textContent = 'Significado';
+    }
+}
+
+botonMostrar8.addEventListener('click', mostrarOcultarDescripcion8);
+
+// BITTERSUITE (9)
+
+const botonMostrar9 = document.querySelector('#mostrar9');
+const significado9 = document.querySelector('#meaning9');
+
+function mostrarOcultarDescripcion9() {
+    if (significado9.style.display === 'none') {
+        significado9.style.display = 'block';
+        botonMostrar9.textContent = 'Ocultar';
+    } else {
+        significado9.style.display = 'none';
+        botonMostrar9.textContent = 'Significado';
+    }
+}
+
+botonMostrar9.addEventListener('click', mostrarOcultarDescripcion9);
+
+// BLUE (10)
+
+const botonMostrar10 = document.querySelector('#mostrar10');
+const significado10 = document.querySelector('#meaning10');
+
+function mostrarOcultarDescripcion10() {
+    if (significado10.style.display === 'none') {
+        significado10.style.display = 'block';
+        botonMostrar10.textContent = 'Ocultar';
+    } else {
+        significado10.style.display = 'none';
+        botonMostrar10.textContent = 'Significado';
+    }
+}
+
+botonMostrar10.addEventListener('click', mostrarOcultarDescripcion10);
